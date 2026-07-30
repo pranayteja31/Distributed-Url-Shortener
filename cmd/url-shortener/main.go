fe63008bfa5e2b08b7bf5ea0204d38e094118156
Pranay Teja <saipranaytejay@gmail.com>
2026-07-30 08:22:41 +0530

    implement graceful shutdown for the server and improve error logging

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,10 +2,16 @@ package main
 
 //this is the main entry point of the application
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
 	"pranayteja31/Urlshortener/internal/config"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -28,12 +34,37 @@ func main() {
 		Handler: router,
 	}
 
-	fmt.Println("starting the server")
-	
-	//start the server with given parameters
-	err := Server.ListenAndServe()
+	fmt.Println("started server..")
+	//enclosing the server to a thread for graeful shutdown
+	done := make(chan os.Signal,1)
+	signal.Notify(done,os.Interrupt,syscall.SIGINT,syscall.SIGTERM) //if any interrup signals arise the value of channel done changes
+
+	go func ()  {
+		//start the server with given parameters
+		err := Server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start the server") // error handling at every step
+		}
+	}()
+
+	<-done //if the chanel receives any message due to interrupt it means the interrup signal arised
+
+	//Adding graceful shutdown - instead of sudden shutdown it checks and then shuts down the server
+	//logic to shutdowm the server because done chan go interrupt signal
+
+	slog.Info("shutting down the server")//logs the info related to shutdown
+	ctx,cancel := context.WithTimeout(context.Background(),5*time.Second) //a timeout of 5s
+	defer cancel() //cleanup practice of created context
+	//defer - runs agter completion of a function
+
+	err := Server.Shutdown(ctx) //shutdown after 5s
 	if err != nil {
-		log.Fatal("failed to start the server") // error handling at every step
+		slog.Error("failed to shutdown",slog.String("error",err.Error()))
 	}
+	
+	
+
+
+
 
 }
