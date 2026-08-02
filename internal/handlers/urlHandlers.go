6b415240a1a4432024582287a2cbf2d877d9db41
Pranay Teja <saipranaytejay@gmail.com>
2026-08-02 14:50:37 +0530

    added url handlers

--- internal/handlers/urlHandlers.go
+++ internal/handlers/urlHandlers.go
@@ -6,7 +6,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Urlshorten(c *gin.Context) {
+func CreateShortUrl(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message":"creates the short url"})
 }
 
+func Redirect(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message":"redirects the short url"})
+}
+
+func GetUrl(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message":"gives the actual url of the short url"})
+}
+
+func ListUrls(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message":"lists all of the short url"})
+}
+func UpdateUrl(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message":"updates the created short url"})
+}
+func DeletUrl(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"message":"deletes the short url"})
+}
