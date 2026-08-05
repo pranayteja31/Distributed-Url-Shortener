5557bf3c76906217ab9767615c4e8a4a5298ccb2
Pranay Teja <saipranaytejay@gmail.com>
2026-08-05 20:07:50 +0530

    fix: standardize comment formatting in route definitions

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -14,15 +14,15 @@ func RegisterRoutes(router *gin.Engine, handler *handlers.URLHandler){
 	//"/api/v1/..."
 	//creation of the short url--
 	url.POST("/create", handler.CreateShortUrl)
-	//get all the details about the url
+	//get all the details about the url--
 	url.GET("/get/:id",handler.GetUrl)
 	//list all urls--
 	url.GET("/list",handler.ListUrls)
-	//update the url
+	//update the url--
 	url.PUT("/update/:id",handler.UpdateUrl)
-	//delete the url
+	//delete the url--
 	url.DELETE("/delete/:id",handler.DeleteUrl)
 
-	//creation of the short url
+	//creation of the short url--
 	router.GET("/:shortCode",handler.Redirect)
 }
